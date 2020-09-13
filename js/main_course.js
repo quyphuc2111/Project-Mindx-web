06ef9f8000bea60ac09d8ccdf3817ab5899d017a
QUYYPHUCC\ASUS <quyphuc211120@gmail.com>
2020-09-13 20:52:54 +0700

    dark mode main course

--- js/main_course.js
+++ js/main_course.js
@@ -1,8 +1,8 @@
 
 var categoryTitle = document.querySelectorAll('.category-title');
 var allCourse = document.querySelectorAll('.all')
-console.log(categoryTitle)
-console.log(allCourse)
+// console.log(categoryTitle)
+// console.log(allCourse)
 
 for(let i =0; i<categoryTitle.length;i++) {
     categoryTitle[i].addEventListener('click', 
@@ -18,7 +18,7 @@ function filterPosts(item) {
                 allCourse[i].parentElement.style.display = 'none';
             }
         }
-        console.log(item.attributes.id.value)
+        // console.log(item.attributes.id.value)
     
 }
 function changeActivePosition(activeItem) {
@@ -26,4 +26,53 @@ function changeActivePosition(activeItem) {
         categoryTitle[i].classList.remove('active')
     }
     activeItem.classList.add('active');
+}
+
+
+var mainCourse = document.getElementById('mainCourse')
+var heading = document.querySelector('#mainCourse .heading')
+var category = document.querySelectorAll('.category-title')
+var courseItem = document.querySelectorAll('.courseItem')
+var title_h3 = document.querySelectorAll('#mainCourse .courseContent h3 a')
+var tc = document.querySelector('#all')
+var a = 0;
+function darkmode() {
+    if(a == 0) {
+        mainCourse.parentElement.style.backgroundColor = '#202020'; 
+        mainCourse.parentElement.style.color = '#c6c6c6'; 
+         heading.style.backgroundColor = '#404040';
+         category.forEach(function(item) {
+             item.classList.add('darkmode')
+             if(item.attributes.id.value == 'all') {
+                 item.classList.remove('darkmode');
+             } 
+             item.parentElement.style.color = '';
+         })
+         courseItem.forEach(function(item) {
+             item.classList.add('darkmode');
+         })
+         title_h3.forEach(function(item) {
+             item.style.color = '#c2c2c2';
+         })
+        tc.parentElement.style.color ='#c4bba9';
+         a = 1;
+    } else {
+        mainCourse.parentElement.style.backgroundColor = '#ffffff'; 
+        mainCourse.parentElement.style.color = '#0d0e0d'; 
+         heading.style.backgroundColor = '#808080';
+         category.forEach(function(item) {
+            item.classList.remove('darkmode');
+            item.parentElement.style.color = 'black';
+            
+        })
+        courseItem.forEach(function(item) {
+            item.classList.remove('darkmode');
+        })
+        title_h3.forEach(function(item) {
+            item.style.color = 'black';
+        })
+
+        a=0;
+    }
+    console.log(a)
 }
